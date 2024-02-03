fe5459f8d79ffbaebb12e260f7572147e7e936f4
a-dehghan <dehghan9156@gmail.com>
2024-02-03 13:43:25 +0330

    [UPD]add url sighnupuser

--- todowoo/urls.py
+++ todowoo/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from todo import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('signup/',views.signupuser,name='signupuser'),
 ]
