bef935d00d842c11a83d2bdae0a45bc083f5ea57
a-dehghan <dehghan9156@gmail.com>
2024-02-04 11:50:40 +0330

    [UPD]edit url accounts

--- todowoo/urls.py
+++ todowoo/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from todo import views
+from django.urls import path,include
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('signup/',views.signupuser,name='signupuser'),
+    path('accounts/',include('accounts.urls')),
 ]
