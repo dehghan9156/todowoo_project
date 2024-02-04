7a22e4b85259412740da29b5b193c18566acf615
a-dehghan <dehghan9156@gmail.com>
2024-02-04 11:36:20 +0330

    [UPD]add form login,signup

--- accounts/forms.py
+++ accounts/forms.py
@@ -23,3 +23,6 @@ class UserSignUpForm(forms.Form):
                 raise forms.ValidationError("The two password fields must match.")
         return cleaned_data
 
+class UserLoginForm(forms.Form):
+    username = forms.CharField(widget =forms.TextInput(attrs={'class':'form-control'}))
+    password = forms.CharField(widget = forms.PasswordInput(attrs={'class':'form-control'}))
