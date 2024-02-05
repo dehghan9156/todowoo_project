8e841a6c0a45e352cfe9b2d51c930a7f3d2f767c
a-dehghan <dehghan9156@gmail.com>
2024-02-05 10:34:27 +0330

    [UPD] add url deletetodo

--- todo/urls.py
+++ todo/urls.py
@@ -4,5 +4,8 @@ app_name = 'todo'
 
 urlpatterns = [
     path('createtodo/',views.createtodo,name='createtodo'),
+    path('currenttodo/', views.currenttodo, name='currenttodo'),
+    path('viewtodo/<int:todo_id>', views.viewtodo, name='viewtodo'),
+    path('deletetodo/<int:todo_id>', views.deletetodo, name='deletetodo'),
 
 ]
