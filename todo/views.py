a4be15be2db127c24c8d7e677037746296a258dc
a-dehghan <dehghan9156@gmail.com>
2024-02-05 10:35:10 +0330

    [UPD] add def delete,viewtodo

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 from .forms import TodoCreateForm
 from django.contrib import messages
 from .models import Todo
@@ -13,7 +13,29 @@ def createtodo(request):
             new_todo.important = form.cleaned_data['important']
             new_todo.save()
             messages.success(request, 'todo created succssfully', 'success')
-            return redirect('accounts :home')
+            return redirect('accounts:home')
     else:
         form = TodoCreateForm()
     return render(request, 'todo/createtodo.html', {'form': form})
+
+def currenttodo(request):
+
+    todos = Todo.objects.filter(user=request.user)
+    return render(request,'todo/currenttodo.html',{'todos':todos})
+
+def viewtodo(request,todo_id):
+    todo = get_object_or_404(Todo,id=todo_id,user=request.user)
+    if request.method == 'POST':
+        form = TodoCreateForm(request.POST,instance = todo)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'User updated succssfully', 'success')
+            return redirect('todo:currenttodo')
+    else:
+        form = TodoCreateForm(instance = todo)
+    return render(request,'todo/viewtodo.html',{'todo':todo , 'form':form})
+
+def deletetodo(request,todo_id):
+    Todo.objects.get(id=todo_id).delete()
+    messages.success(request,'User deleted succssfully','success')
+    return redirect('todo:currenttodo')
