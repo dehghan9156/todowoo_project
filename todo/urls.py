52eb63040a8f505133be5f6904ca88adf822d007
a-dehghan <dehghan9156@gmail.com>
2024-02-05 11:45:09 +0330

    [UPD]rename url viewtodo

--- todo/urls.py
+++ todo/urls.py
@@ -5,7 +5,7 @@ app_name = 'todo'
 urlpatterns = [
     path('createtodo/',views.createtodo,name='createtodo'),
     path('currenttodo/', views.currenttodo, name='currenttodo'),
-    path('viewtodo/<int:todo_id>', views.viewtodo, name='viewtodo'),
+    path('updatetodo/<int:todo_id>', views.updatetodo, name='updatetodo'),
     path('deletetodo/<int:todo_id>', views.deletetodo, name='deletetodo'),
 
 ]
